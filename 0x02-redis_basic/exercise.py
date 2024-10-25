a5f65ee4e6505733dd861a57d5dd61b42d97f566
otienojoseph <otienoj.cruz@gmail.com>
2024-10-25 09:42:40 +0300

    Read from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Cache class that creates an instance of a Redis client"""
-from typing import Union
+from collections.abc import Callable
+from typing import Optional, Union
 import uuid
 import redis
 
@@ -28,3 +29,24 @@ class Cache():
         self._redis.set(rand_key, data)
 
         return rand_key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[
+            Union[str, bytes, int, float]]:
+        """
+        Retrieve data from Redis and optionally apply a conversion
+        function
+
+        Args:
+            key (str): The key for the data to be retrieved
+            fn (Callable, optional): A function to convert the data
+            to the desired type
+
+        Returns:
+            Optional[Union[str, int, float, bytes]]: The retrieved and
+            possibly converted data, or None
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+
+        return fn(data) if fn else data
